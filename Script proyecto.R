f7f233b8f51e30b8d670b5ac6fb049f4160ce6b2
Jesús Barrón <pbarron20@alumnos.uaq.mx>
2022-12-12 18:39:21 -0600

    Create Script proyecto.R

---
+++ Script proyecto.R
@@ -0,0 +1,14 @@
+#Preguntas a realizar
+
+#¿Lleva demografía? Si o no
+#¿Es con vectores? Si o no
+#¿Es homógeneo? Si o no
+#¿Es heterógeneo? SI o no
+
+#Tipos de Modelo
+#SI
+#SIS
+#SIR
+#SIRS
+#SEIR
+#SEIRS
