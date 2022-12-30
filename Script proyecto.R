4bb8b612952e0c49daa9297b0eb84ead0f96c4ba
Jesús Barrón <pbarron20@alumnos.uaq.mx>
2022-12-29 21:58:07 -0600

    ¿Nueva función?
    Se buscó crear una nueva función para evadir el error:
     "Error in -beta : invalid argument to unary operator"
    Pero en el nuevo modelo se obtiene el error:
    "Error in p1 == SI :
      comparison (1) is possible only for atomic and list types"

--- Script proyecto.R
+++ Script proyecto.R
@@ -237,19 +237,19 @@ epi_model <- function(){
     if(p1.a == FALSE){
       SIS <- function(t,state,parameters){
         with(as.list(c(state, parameters)), {
-          dS <- - beta2*dapri*dapri1 + gama1*dapri1
-          dI <- beta1*dapri*dapri1 - gama2*dapri1
+          dS <- -(beta2*dapri*dapri1) + gama1*dapri1
+          dI <- beta1*dapri*dapri1 - (gama2*dapri1)
           list(c(dS, dI))
         })
       }
       beta1 <- readline(prompt = "Cuál es el valor de beta?" )
       beta1 <- as.numeric(beta1)
       beta2 <- readline(prompt = "Cuál es el valor de beta?" )
-      beta2 <- as.numeric(beta1)
+      beta2 <- as.numeric(beta2)
       gama1 <- readline(prompt = "Cuál es el valor de gama?" )
       gama1 <- as.numeric(gama1)
       gama2 <- readline(prompt = "Cuál es el valor de gama?" )
-      gama2 <- as.numeric(gama1)
+      gama2 <- as.numeric(gama2)
       dapri <- readline(prompt = "Cuál es el valor de los susceptibles?" )
       dapri <- as.numeric(dapri)
       dapri1 <- readline(prompt = "Cuál es el valor de los infectados?" )
@@ -262,8 +262,8 @@ epi_model <- function(){
     }else if(p1.a == T){
       SIS <- function(t,state,parameters){
         with(as.list(c(state, parameters)), {
-          dS <- mu*N - beta2*dapri*dapri1 + gama1*dapri1 - mu1*dapri
-          dI <- beta1*dapri*dapri1 - gama2*dapri1 - mu2*dapri1
+          dS <- mu*N - (beta2*dapri*dapri1) + gama1*dapri1 - (mu1*dapri)
+          dI <- beta1*dapri*dapri1 - (gama2*dapri1) - (mu2*dapri1)
           list(c(dS, dI))
         })
       }
@@ -274,11 +274,11 @@ epi_model <- function(){
       beta1 <- readline(prompt = "Cuál es el valor de beta?" )
       beta1 <- as.numeric(beta1)
       beta2 <- readline(prompt = "Cuál es el valor de beta?" )
-      beta2 <- as.numeric(beta1)
+      beta2 <- as.numeric(beta2)
       gama1 <- readline(prompt = "Cuál es el valor de gama?" )
       gama1 <- as.numeric(gama1)
       gama2 <- readline(prompt = "Cuál es el valor de gama?" )
-      gama2 <- as.numeric(gama1)
+      gama2 <- as.numeric(gama2)
       dapri <- readline(prompt = "Cuál es el valor de S?" )
       dapri <- as.numeric(dapri)
       mu1 <- readline(prompt = "¿Cuál es la tasa de muerte de los susceptibles?" )
@@ -297,3 +297,186 @@ epi_model <- function(){
 }
 
 #Ya medio jala
+
+#Nuevo Modelo 
+modelando <- function(){
+  library(deSolve)
+  p1 <- readline(prompt = "¿El modelo deseado es un SI, SIS, SIR, SIRS, SEIR, SEIRS?" )
+  p1 <- as.character(p1)
+  p2 <- readline(prompt = "¿El modelo lleva demografía? En caso de llevar, contestar con T o  TRUE, si no es el caso, contestar con F o False ")
+  p2 <- as.logical(p2)
+  if(p1 == SI & p2 == T){
+    SI <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N - beta*S*I - mu1*S
+        dI <- beta*S*I - mu2*I
+        list(c(dS, dI))
+      })
+    }
+    mu <- readline(prompt = "¿Cuál es la tasa de crecimiento de los nacimientos?" )
+    mu <- as.numeric(mu)
+    N <- readline(prompt = "¿Cuál es el valor de N?" )
+    N <- as.numeric(N)
+    beta <- readline(prompt = "¿Cuál es el valor de beta?" )
+    beta <- as.numeric(beta)
+    S <- readline(prompt = "¿Cuál es el valor de S?" )
+    S <- as.numeric(S)
+    mu1 <- readline(prompt = "¿Cuál es la tasa de muerte de los susceptibles?" )
+    mu1 <- as.numeric(mu1)
+    I <- readline(prompt = "¿Cuál es el valor de I?" )
+    I <- as.numeric(I)
+    mu2 <- readline(prompt = "¿Cuál es la tasa de muerte de los infectados?" )
+    mu2 <- as.numeric(mu2)
+    parametro2 <- c(beta, mu, mu1, mu2)
+    daprincipio <- c(S, I)
+    taaim <- seq(c(0, 20, by = 0.001))
+    outo <- ode(daprincipio, taaim, SI, parametro2)
+    return(outo)
+  }else if(p1 == SI & p2 == F){
+    SI <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- - beta*S*I
+        dI <- beta*S*I
+        list(c(dS, dI))
+      })
+    }
+    beta <- readline(prompt = "¿Cuál es el valor de beta?" )
+    beta <- as.numeric(beta)
+    S <- readline(prompt = "¿Cuál es el valor de S?" )
+    S <- as.numeric(S)
+    I <- readline(prompt = "¿Cuál es el valor de I?" )
+    I <- as.numeric(I)
+    parametro2 <- c(beta)
+    daprincipio <- c(S, I)
+    taaim <- seq(c(0, 20, by = 0.001))
+    outo <- ode(daprincipio, taaim, SI, parametro2)
+    return(outo)
+  }else if(p1 == SIS & p2 == T){
+    SIS <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N - beta*S*I + gama*I - mu1*S
+        dI <- beta*S*I - gama*I - mu2*I
+        list(c(dS, dI))
+      })
+    }
+    mu <- readline(prompt = "¿Cuál es la tasa de crecimiento de los nacimientos?" )
+    mu <- as.numeric(mu)
+    N <- readline(prompt = "¿Cuál es el valor de N?" )
+    N <- as.numeric(N)
+    beta <- readline(prompt = "Cuál es el valor de beta?" )
+    beta <- as.numeric(beta)
+    gama <- readline(prompt = "Cuál es el valor de gama?" )
+    gama <- as.numeric(gama)
+    S <- readline(prompt = "Cuál es el valor de S?" )
+    S <- as.numeric(S)
+    mu1 <- readline(prompt = "¿Cuál es la tasa de muerte de los susceptibles?" )
+    mu1 <- as.numeric(mu1)
+    I <- readline(prompt = "Cuál es el valor de I?" )
+    I <- as.numeric(I)
+    mu2 <- readline(prompt = "¿Cuál es la tasa de muerte de los infectados?" )
+    mu2 <- as.numeric(mu2)
+    parametro2 <- c(beta, gama, mu, N, mu1, mu2)
+    daprincipio <- c(S, I)
+    taaim <- seq(c(0, 20, by = 0.001))
+    outo <- ode(daprincipio, taaim, SIS, parametro2)
+    return(outo)
+  }else if(p1 == SIS & p2 == F){
+    SIS <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- - beta*S*I + gama*I
+        dI <- beta*S*I - gama*I
+        list(c(dS, dI))
+      })
+    }
+    beta <- readline(prompt = "Cuál es el valor de beta?" )
+    beta <- as.numeric(beta)
+    gama <- readline(prompt = "Cuál es el valor de gama?" )
+    gama <- as.numeric(gama)
+    S <- readline(prompt = "Cuál es el valor de S?" )
+    S <- as.numeric(S)
+    I <- readline(prompt = "Cuál es el valor de I?" )
+    I <- as.numeric(I)
+    parametro2 <- c(beta, gama)
+    daprincipio <- c(S, I)
+    taaim <- seq(c(0, 20, by = 0.001))
+    outo <- ode(daprincipio, taaim, SIS, parametro2)
+    return(outo)
+  }else if(p1 == SIR & p2 == T){
+    SIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N -beta*S*I - mu*S
+        dI <- beta*S*I - gama*I - mu*I
+        dR <- gama*I - mu*R
+        list(c(dS, dI, dR))
+      })
+    }
+  }else if(p1 == SIR & p2 == F){
+    SIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- -beta*S*I 
+        dI <- beta*S*I - gama*I
+        dR <- gama*I
+        list(c(dS, dI, dR))
+      })
+    }
+  }else if(p1 == SIRS & p2 == T){
+    SIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N -beta*S*I + delta*R - mu*S
+        dI <- beta*S*I - gama*I - mu*I
+        dR <- gama*I - delta*R - mu*R
+        list(c(dS, dI, dR))
+      })
+    }
+  }else if(p1 == SIRS & p2 == F){
+    SIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- -beta*S*I + delta*R
+        dI <- beta*S*I - gama*I
+        dR <- gama*I - delta*R
+        list(c(dS, dI, dR))
+      })
+    }
+  }else if(p1 == SEIR & p2 == T){
+    SEIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N -beta*S*I -mu*S 
+        dE <- beta*S*I - delta*I - mu*E
+        dI <- delta*I - gama*R - mu*I
+        dR <- gama*R -  mu*R
+        list(c(dS, dE, dI, dR))
+      })
+    }
+  }else if(p1 == SEIR & p2 == F){
+    SEIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- -beta*S*I 
+        dE <- beta*S*I - delta*I
+        dI <- delta*I - gama*R
+        dR <- gama*R
+        list(c(dS, dE, dI, dR))
+      })
+    }
+  }else if(p1 == SEIRS & p2 == T){
+    SEIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- mu*N -beta*S*I + ro*R - mu*S
+        dE <- beta*S*I - delta*I -mu*E
+        dI <- delta*I - gama*R - mu*I
+        dR <- gama*R - ro*R - mu*R
+        list(c(dS, dE, dI, dR))
+      })
+    }
+  }else if(p1 == SEIRS & p2 == F){
+    SEIR <- function(t,state,parameters){
+      with(as.list(c(state, parameters)), {
+        dS <- -beta*S*I + ro*R
+        dE <- beta*S*I - delta*I
+        dI <- delta*I - gama*R
+        dR <- gama*R - ro*R
+        list(c(dS, dE, dI, dR))
+      })
+    }
+  }
+  
+}
